60f96875c16928049f67d38140890f68fd798254
Sampson Zhao <sampson.zhao@mail.utoronto.ca>
2022-02-06 22:31:14 -0500

    Final Edits

--- scripts/02-data_clean.R
+++ scripts/02-data_clean.R
@@ -28,77 +28,44 @@ filtered_data <-
          User.Type)
 rm(raw_data)
 
+### Add new column to determine if trip was considered long or not
 filtered_data <- 
   filtered_data |>
   mutate(Long_Trip = ifelse(Trip..Duration > 900, 'Yes','No'))
 
-filtered_data |>
-  ggplot(mapping = aes(x = factor(Start.Station.Id))) +
-  geom_bar()
-
+### Filter data based on random number generator
 start_selected_data <-
   filtered_data |>
   filter (Start.Station.Id %in% 
                   c('7249', '7338', '7293', '7662', '7107', '7428'))
-
 end_selected_data <-
   filtered_data |>
   filter (End.Station.Id %in% 
             c('7249', '7338', '7293', '7662', '7107', '7428'))
+
+# There was some issue with the data set having extra entries, so the last 2
+# entries were removed so that the length of both start and end data are the same. 
 end_selected_data <-
   end_selected_data|>
   slice(1:1012)
 
-start_selected_data |>
-  ggplot(mapping = aes(x = factor(Start.Station.Id), fill = User.Type)) + 
-  geom_bar(position="dodge")+
-  labs(title = 'Number of Users at 6 Random Start Stations in December 2021',
-       x = 'Starting Station', y = 'Number of Riders') +
-  geom_text(stat='count', aes(label=after_stat(count)), 
-            position = position_dodge(width = 0.9), vjust = -1)
 
-end_selected_data |>
-  ggplot(mapping = aes(x = factor(End.Station.Id), fill = User.Type)) + 
-  geom_bar(position="dodge")+
-  labs(title = 'Number of Users at 6 Random End Stations in December 2021',
-       x = 'Ending Station', y = 'Number of Riders') +
-  geom_text(stat='count', aes(label=after_stat(count)), 
-            position = position_dodge(width = 0.9), vjust = -1)
-
-### Merge Count data for start and end station 
-count_data_start <-
-  start_selected_data|>
-  count(Start.Station.Id, name = 'Start.Station')
-
-count_data_start <-
-  count_data_start |>
-  rename(Station.ID=Start.Station.Id)
-
-count_data_end <-
-  end_selected_data |>
-  count(End.Station.Id, name = 'End.Station')
-
-count_data_end <-
-  count_data_end |>
-  rename(Station.ID=End.Station.Id)
-
-count_data <-
-  merge(count_data_start, count_data_end, by = 'Station.ID')
-
-rm(count_data_start,count_data_end)
-
-count_data_2 <- melt(count_data, id.vars='Station.ID')
-
-### following code was found at: https://stackoverflow.com/questions/42820677/ggplot-bar-plot-side-by-side-using-two-variables
-
-count_data_2 |>
-  ggplot(mapping = aes(x = factor(Station.ID), y = value, fill = variable)) + 
-  geom_bar(stat='identity', position='dodge')+
-  labs(title = 'Number of Users at 6 Random End Stations in December 2021',
-       x = 'Station ID', y = 'Number of Riders') +
-  scale_fill_discrete(name = "Ridership Activity", 
-                      labels = c("Pickup", "Dropoff"))
+### Code here is experimental for plot generation, no need for this.
   
+#start_selected_data |>
+#  ggplot(mapping = aes(x = factor(Start.Station.Id), fill = User.Type)) + 
+#  geom_bar(position="dodge")+
+#  labs(title = 'Number of Users at 6 Random Start Stations in December 2021',
+#       x = 'Starting Station', y = 'Number of Riders') +
+#  geom_text(stat='count', aes(label=after_stat(count)), 
+#            position = position_dodge(width = 0.9), vjust = -1)
 
+#end_selected_data |>
+#  ggplot(mapping = aes(x = factor(End.Station.Id), fill = User.Type)) + 
+#  geom_bar(position="dodge")+
+#  labs(title = 'Number of Users at 6 Random End Stations in December 2021',
+#       x = 'Ending Station', y = 'Number of Riders') +
+#  geom_text(stat='count', aes(label=after_stat(count)), 
+#            position = position_dodge(width = 0.9), vjust = -1)
 
 
